463ed583f92bbc986d3a006dabeb912d6c6a1508
AKennedy <akennedy1502@gmail.com>
2020-03-08 16:12:43 -0500

    part2 data layer

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+
+    private String location;
+
+    public Employer(){
+
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        location = location;
+    }
+
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public Skill(){
+
+    }
+
+    private String extraSkillDescription;
+
+    public String getExtraSkillDescription() {
+        return extraSkillDescription;
+    }
+
+    public void setExtraSkillDescription(String extraSkillDescription) {
+        this.extraSkillDescription = extraSkillDescription;
+    }
+
+
 
 }
