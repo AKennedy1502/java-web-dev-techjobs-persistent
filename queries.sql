38c085bd865b910fbc4985d5abd8d1fb34a2ec37
AKennedy <akennedy1502@gmail.com>
2020-03-08 14:12:27 -0500

    finished part 1

--- queries.sql
+++ queries.sql
@@ -1,10 +1,11 @@
 ## Part 1: Test it with SQL
 CREATE TABLE job (
-id INT,
-employer VARCHAR,
-name VARCHAR,
-skills VARCHAR
+id INTEGER,
+employer VARCHAR(100),
+name VARCHAR(50),
+skills VARCHAR(100)
 );
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
